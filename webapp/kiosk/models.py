11c54108f5f906db1587a79d59ce7ea425edfd22
Lyndon <lyndon@onthelink.net>
2025-03-19 00:00:42 +1300

    Cart system and buisness logic starting to come together

--- webapp/kiosk/models.py
+++ webapp/kiosk/models.py
@@ -11,17 +11,23 @@ class Product(models.Model):
 class User(models.Model):
     card_id = models.CharField(max_length=12)
     name = models.CharField(max_length=64)
-    date_created = models.DateTimeField("date created")
+    date_created = models.DateTimeField("date created", auto_now_add=True, blank=True)
 
     def __str__(self):
         return self.name
 
 class Cart(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    datetime_created = models.DateTimeField("date created")
-    datetime_completed = models.DateTimeField("date completed")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    datetime_created = models.DateTimeField("date created", auto_now_add=True, blank=True)
+    datetime_completed = models.DateTimeField("date completed", blank=True, null=True)
+
+    def __str__(self):
+        return ("id: %s, user_id: %s, datetime_created: %s, datetime_completed: %s") % (self.id, self.user_id, self.datetime_created, self.datetime_completed)
 
 class CartItem(models.Model):
     cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    datetime = models.DateTimeField("date added")
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    datetime_created = models.DateTimeField("date created", auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return ("id: %s, product_id: %s, datetime_created: %s") % (self.id, self.product_id, self.datetime_created)
--- webapp/kiosk/views.py
+++ webapp/kiosk/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
-from .models import Product
+from .models import Product, Cart, CartItem
 
 def index(request):
     product_list = Product.objects.all()
@@ -9,6 +9,36 @@ def index(request):
     return render(request, 'kiosk/index.html', context)
 
 def add_to_cart(request):
+
     if request.method == 'POST':
-         data = json.load(request)
-    return JsonResponse({'returned': data})
+        request_data = json.load(request)
+
+        # If there is no cart_id set yet we are going to need one, otherwise use whet we were given
+        if "cart_id" not in request_data or request_data['cart_id'] == -1:
+            cart = Cart()
+            cart.save()
+        else:
+            cart = Cart.objects.get(pk=request_data['cart_id'])
+
+        # Create a cartitem with the product selected
+        added_product = Product.objects.get(pk=request_data['product_id'])
+        added_product.save()
+        cart_item = CartItem(cart_id=cart, product_id=added_product)
+        cart_item.save()
+
+    return JsonResponse(get_cart_content(cart.id))
+
+
+def get_cart_content(cart_id):
+
+    # Get the cart in question and all items in it
+    cart = Cart.objects.get(pk=cart_id)
+    cart_items = list(CartItem.objects.filter(cart_id=cart))
+
+    # Send back a web consumable objects
+    return {
+        'id': cart.id,
+        'cart_items': [{'name': cart_item.product_id.name,'price': cart_item.product_id.price} for cart_item in cart_items],
+        'total_price': sum([cart_item.product_id.price for cart_item in cart_items])
+    }
+
